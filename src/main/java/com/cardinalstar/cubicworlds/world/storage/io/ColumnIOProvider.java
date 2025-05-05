957305c7fc4b4c9fac4f736839167bd2cf5ea87f
rjnasers <rjnasers@iastate.edu>
2025-05-05 23:55:57 +0200

    Getting there on some more stuff. Progress is slow

--- src/main/java/com/cardinalstar/cubicworlds/world/storage/io/ColumnIOProvider.java
+++ src/main/java/com/cardinalstar/cubicworlds/world/storage/io/ColumnIOProvider.java
@@ -3,25 +3,34 @@ package com.cardinalstar.cubicworlds.world.storage.io;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraftforge.common.util.AsynchronousExecutor;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class ColumnIOProvider implements AsynchronousExecutor.CallBackProvider<QueuedColumn, Chunk, Runnable, RuntimeException>
 {
+    private final AtomicInteger threadNumber = new AtomicInteger(1);
+
     @Override
-    public Chunk callStage1(QueuedColumn parameter) throws RuntimeException {
+    public Chunk callStage1(QueuedColumn parameter) throws RuntimeException
+    {
         return null;
     }
 
     @Override
-    public void callStage2(QueuedColumn parameter, Chunk object) throws RuntimeException {
+    public void callStage2(QueuedColumn parameter, Chunk object) throws RuntimeException
+    {
 
     }
 
     @Override
-    public void callStage3(QueuedColumn parameter, Chunk object, Runnable callback) throws RuntimeException {
-
+    public void callStage3(QueuedColumn parameter, Chunk object, Runnable callback) throws RuntimeException
+    {
+        callback.run();
     }
 
     @Override
-    public Thread newThread(Runnable r) {
-        return null;
+    public Thread newThread(Runnable runnable) {
+        Thread thread = new Thread(runnable, "Cube I/O Executor Thread-" + threadNumber.getAndIncrement());
+        thread.setDaemon(true);
+        return thread;
     }
 }
--- src/main/java/com/cardinalstar/cubicworlds/world/storage/io/CubicIOExecutor.java
+++ src/main/java/com/cardinalstar/cubicworlds/world/storage/io/CubicIOExecutor.java
@@ -1,47 +1,64 @@
 package com.cardinalstar.cubicworlds.world.storage.io;
 
+import com.cardinalstar.cubicworlds.server.CubeProviderServer;
+import com.cardinalstar.cubicworlds.server.CubicAnvilChunkLoader;
 import com.cardinalstar.cubicworlds.world.cube.Cube;
+import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 
-import net.minecraftforge.common.chunkio.QueuedChunk;
 import net.minecraftforge.common.util.AsynchronousExecutor;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.ThreadPoolExecutor;
 
-
-public class CubeIOExecutor
+public class CubicIOExecutor
 {
     private static final int BASE_THREADS = 1;
     private static final int PLAYERS_PER_THREAD = 50;
 
     private static final AsynchronousExecutor<QueuedColumn, Chunk, Runnable, RuntimeException> columnInstance = new AsynchronousExecutor<QueuedColumn, Chunk, Runnable, RuntimeException>(new ColumnIOProvider(), BASE_THREADS);
-    private static final AsynchronousExecutor<QueuedColumn, Cube, Runnable, RuntimeException> cubeInstance = new AsynchronousExecutor<QueuedColumn, Cube, Runnable, RuntimeException>(new CubeIOProvider(), BASE_THREADS);
+    private static final AsynchronousExecutor<QueuedCube, Cube, Runnable, RuntimeException> cubeInstance = new AsynchronousExecutor<QueuedCube, Cube, Runnable, RuntimeException>(new CubeIOProvider(), BASE_THREADS);
 
 
-    public static net.minecraft.world.chunk.Chunk syncChunkLoad(net.minecraft.world.World world, net.minecraft.world.chunk.storage.AnvilChunkLoader loader, net.minecraft.world.gen.ChunkProviderServer provider, int x, int z) {
+    public static Chunk syncChunkLoad(World world, CubicAnvilChunkLoader loader, CubeProviderServer provider, int x, int z) {
         return columnInstance.getSkipQueue(new QueuedColumn(x, z, loader, world, provider));
     }
 
-    public static void queueChunkLoad(net.minecraft.world.World world, net.minecraft.world.chunk.storage.AnvilChunkLoader loader, net.minecraft.world.gen.ChunkProviderServer provider, int x, int z, Runnable runnable) {
+    public static void queueChunkLoad(World world, CubicAnvilChunkLoader loader, CubeProviderServer provider, int x, int z, Runnable runnable) {
         columnInstance.add(new QueuedColumn(x, z, loader, world, provider), runnable);
     }
 
     // Abuses the fact that hashCode and equals for QueuedChunk only use world and coords
-    public static void dropQueuedChunkLoad(net.minecraft.world.World world, int x, int z, Runnable runnable) {
+    public static void dropQueuedChunkLoad(World world, int x, int z, Runnable runnable) {
         columnInstance.drop(new QueuedColumn(x, z, null, world, null), runnable);
     }
 
-    public static void adjustPoolSize(int players) {
-        int size = Math.max(BASE_THREADS, (int) Math.ceil(players / PLAYERS_PER_THREAD));
+    public static void adjustColumnPoolSize(int players) {
+        int size = Math.max(BASE_THREADS, (int) Math.ceil((double) players / PLAYERS_PER_THREAD));
         columnInstance.setActiveThreads(size);
+    }
+
+    public static Cube syncCubeLoad(World world, CubicAnvilChunkLoader loader, CubeProviderServer provider, int x, int y, int z) {
+        return cubeInstance.getSkipQueue(new QueuedCube(x, y, z, loader, world, provider));
+    }
+
+    public static void queueCubeLoad(World world, CubicAnvilChunkLoader loader, CubeProviderServer provider, int x, int y, int z, Runnable runnable) {
+        cubeInstance.add(new QueuedCube(x, y, z, loader, world, provider), runnable);
+    }
+
+    // Abuses the fact that hashCode and equals for QueuedChunk only use world and coords
+    public static void dropQueuedCubeLoad(World world, int x, int y, int z, Runnable runnable) {
+        cubeInstance.drop(new QueuedCube(x, y, z, null, world, null), runnable);
+    }
+
+    public static void adjustCubePoolSize(int players) {
+        int size = Math.max(BASE_THREADS, (int) Math.ceil((double) players / PLAYERS_PER_THREAD));
+        cubeInstance.setActiveThreads(size);
     }
 
     public static void tick() {
         columnInstance.finishActive();
+        cubeInstance.finishActive();
     }
+
+
 
 }
