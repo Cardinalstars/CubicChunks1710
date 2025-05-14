8721a12bf5be2910729fc954d4d6ca83d009b5b3
rjnasers <rjnasers@iastate.edu>
2025-05-14 21:30:18 +0200

    More config fixes

--- src/main/java/com/cardinalstar/cubicchunks/command/SubCommandBase.java
+++ src/main/java/com/cardinalstar/cubicchunks/command/SubCommandBase.java
@@ -1,8 +1,11 @@
 package com.cardinalstar.cubicchunks.command;
 
 import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.ChatComponentTranslation;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
@@ -14,11 +17,15 @@ import java.util.Map;
 /**
  * This class was partially taken from forge 1.12 and rewired to fit my needs.
  */
-public abstract class SubCommandBase extends CommandBase
+public abstract class SubCommandBase extends CubicCommandBase
 {
 
     private final Map<String, ICommand> commandMap = new HashMap<>();
     private final Map<String, ICommand> commandAliasMap = new HashMap<>();
+
+    public SubCommandBase(PermissionLevel permissionLevelRequired) {
+        super(permissionLevelRequired);
+    }
 
     public void addSubcommand(ICommand commandToAdd)
     {
@@ -32,7 +39,29 @@ public abstract class SubCommandBase extends CommandBase
     @Override
     public void processCommand(ICommandSender sender, String[] args)
     {
+        if (args.length < 1)
+        {
+            String subCommandsString = getAvailableSubCommandsString(sender);
+            sender.addChatMessage(new ChatComponentTranslation("commands.tree_base.available_subcommands", subCommandsString));
+        }
+        else
+        {
+            ICommand cmd = getSubCommand(args[0]);
 
+            if(cmd == null)
+            {
+                String subCommandsString = getAvailableSubCommandsString(sender);
+                throw new CommandException("commands.tree_base.invalid_cmd.list_subcommands", args[0], subCommandsString);
+            }
+            else if(!cmd.canCommandSenderUseCommand(sender))
+            {
+                throw new CommandException("commands.generic.permission");
+            }
+            else
+            {
+                cmd.processCommand(sender, shiftArgs(args));
+            }
+        }
     }
 
     public Collection<ICommand> getSubCommands()
@@ -67,16 +96,41 @@ public abstract class SubCommandBase extends CommandBase
             }
 
             keys.sort(null);
-            return getListOfStringsMatchingLastWord(keys, args);
+            return getListOfStringsFromIterableMatchingLastWord(args, keys);
         }
 
         ICommand cmd = getSubCommand(args[0]);
 
         if(cmd != null)
         {
-            return cmd.getTabCompletions(server, sender, shiftArgs(args), pos);
+            return cmd.addTabCompletionOptions(sender, shiftArgs(args));
         }
 
-        return super.getTabCompletions(server, sender, args, pos);
+        return super.addTabCompletionOptions(sender, args);
+    }
+
+    private static String[] shiftArgs(@Nullable String[] s)
+    {
+        if(s == null || s.length == 0)
+        {
+            return new String[0];
+        }
+
+        String[] s1 = new String[s.length - 1];
+        System.arraycopy(s, 1, s1, 0, s1.length);
+        return s1;
+    }
+
+    private String getAvailableSubCommandsString(ICommandSender sender)
+    {
+        Collection<String> availableCommands = new ArrayList<>();
+        for (ICommand command : getSubCommands())
+        {
+            if (command.canCommandSenderUseCommand(sender))
+            {
+                availableCommands.add(command.getCommandName());
+            }
+        }
+        return CommandBase.joinNiceStringFromCollection(availableCommands);
     }
 }
