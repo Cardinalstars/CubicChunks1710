139664a86227fbd67dafe8c7bf41529e9311d95c
Ryan Nasers <ryannasersgig@gmail.com>
2025-08-15 21:10:34 +0200

    Going through some IO Stuff

--- src/main/java/com/cardinalstar/cubicchunks/server/CubicAnvilChunkLoader.java
+++ src/main/java/com/cardinalstar/cubicchunks/server/CubicAnvilChunkLoader.java
@@ -1,80 +1,130 @@
+/*
+ *  This file is part of Cubic Chunks Mod, licensed under the MIT License (MIT).
+ *
+ *  Copyright (c) 2015-2021 OpenCubicChunks
+ *  Copyright (c) 2015-2021 contributors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
 package com.cardinalstar.cubicchunks.server;
 
-import com.cardinalstar.cubicchunks.core.world.cube.Cube;
+import java.io.File;
+import java.io.IOException;
+import java.util.function.Supplier;
+
+import javax.annotation.Nullable;
+
+import com.cardinalstar.cubicchunks.CubicChunks;
+import com.cardinalstar.cubicchunks.api.IColumn;
+import com.cardinalstar.cubicchunks.api.ICube;
+import com.cardinalstar.cubicchunks.server.chunkio.ICubeIO;
+import com.cardinalstar.cubicchunks.util.ChunkPos;
+import com.cardinalstar.cubicchunks.world.cube.Cube;
+import com.cardinalstar.cubicchunks.world.cube.ICubeProviderInternal;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.ChunkCoordIntPair;
-import net.minecraft.world.MinecraftException;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.storage.AnvilChunkLoader;
 
-import java.io.File;
-import java.io.IOException;
-
 public class CubicAnvilChunkLoader extends AnvilChunkLoader {
-    public CubicAnvilChunkLoader(File file) {
-        super(file);
+
+    private ICubeIO cubeIOValue;
+    private final Supplier<ICubeIO> cubeIOSource;
+
+    // cubeIO needs to be supplier and lazy initialized because of the cubic chunks initialization order
+    // AnvilChunkLoader is constructed in CubeProviderServer constructor, which is before CubeIO
+    // which is inside the CubeProviderServer exists.
+    public CubicAnvilChunkLoader(File chunkSaveLocationIn, Supplier<ICubeIO> cubeIO) {
+        super(chunkSaveLocationIn);
+        this.cubeIOSource = cubeIO;
     }
 
-    @Override
-    public Chunk loadChunk(World world, int x, int z) throws IOException
-    {
-        Object[] data = this.loadChunk__Async(world, x, z);
-
-        if (data != null)
-        {
-            Chunk chunk = (Chunk) data[0];
-            NBTTagCompound nbttagcompound = (NBTTagCompound) data[1];
-            this.loadEntities(world, nbttagcompound.getCompoundTag("Level"), chunk);
-            return chunk;
+    private ICubeIO getCubeIO() {
+        if (cubeIOValue == null) {
+            cubeIOValue = cubeIOSource.get();
         }
-
-        return null;
+        return cubeIOValue;
     }
 
-    @Override
-    public Object[] loadChunk__Async(World p_75815_1_, int p_75815_2_, int p_75815_3_) throws IOException
-    {
-        return null;
+    @Override @Nullable public Chunk loadChunk(World worldIn, int x, int z) throws IOException {
+        ICubeIO.PartialData<Chunk> data = ((ICubeProviderInternal.Server) worldIn.getChunkProvider()).getCubeIO().loadColumnAsyncPart(worldIn, x, z);
+        ((ICubeProviderInternal.Server) worldIn.getChunkProvider()).getCubeIO().loadColumnSyncPart(data);
+        return data.getObject();
     }
 
-    public Cube loadCube(World world) throws IOException
-    {
-
+    @Override @Nullable public Object[] loadChunk__Async(World worldIn, int x, int z) throws IOException {
+        ICubeIO.PartialData<Chunk> data = ((ICubeProviderInternal.Server) worldIn.getChunkProvider()).getCubeIO().loadColumnAsyncPart(worldIn, x, z);
+        return new Object[]{data.getObject(), data.getNbt()};
     }
 
-
-    @Override
-    protected Chunk checkedReadChunkFromNBT(World p_75822_1_, int p_75822_2_, int p_75822_3_, NBTTagCompound p_75822_4_)
-    {
-        return null;
+    @Override public boolean chunkExists(World world, int x, int z) {
+        return this.getCubeIO().columnExists(x, z);
     }
 
-    @Override
-    protected Object[] checkedReadChunkFromNBT__Async(World p_75822_1_, int p_75822_2_, int p_75822_3_, NBTTagCompound p_75822_4_)
-    {
-        return null;
+    @Override @Nullable protected Chunk checkedReadChunkFromNBT(World worldIn, int x, int z, NBTTagCompound compound) {
+        throw new UnsupportedOperationException();
     }
 
-    @Override
-    public void saveChunk(World p_75816_1_, Chunk p_75816_2_) throws MinecraftException, IOException
-    {
-
+    @Override @Nullable protected Object[] checkedReadChunkFromNBT__Async(World worldIn, int x, int z, NBTTagCompound compound) {
+        throw new UnsupportedOperationException();
     }
 
-    @Override
-    protected void addChunkToPending(ChunkCoordIntPair p_75824_1_, NBTTagCompound p_75824_2_)
-    {
+    @Override public void saveChunk(World worldIn, Chunk chunkIn) {
+        getCubeIO().saveColumn(chunkIn);
 
+        for (ICube cube : ((IColumn) chunkIn).getLoadedCubes()) {
+            getCubeIO().saveCube((Cube) cube);
+        }
     }
 
-    @Override
-    public boolean writeNextIO()
-    {
-        return false;
+    @Override protected void addChunkToPending(ChunkCoordIntPair pos, NBTTagCompound compound) {
+        throw new UnsupportedOperationException();
     }
 
+    @Override public boolean writeNextIO() {
+        return getCubeIO().writeNextIO();
+    }
 
+    @Override public void saveExtraChunkData(World worldIn, Chunk chunkIn) {
+    }
 
+    @Override public void chunkTick() {
+    }
 
+    /**
+     * Flushes all pending chunks fully back to disk
+     */
+    @Override public void saveExtraData() {
+        try {
+            getCubeIO().flush();
+        } catch (IOException e) {
+            CubicChunks.LOGGER.catching(e);
+        }
+    }
+
+    @Override public void loadEntities(World worldIn, NBTTagCompound compound, Chunk chunk) {
+        throw new UnsupportedOperationException();
+    }
+//
+//    @Override public int getPendingSaveCount() {
+//        // guess what the right value is?
+//        return getCubeIO().getPendingColumnCount() + getCubeIO().getPendingCubeCount() / 16;
+//    }
 }
