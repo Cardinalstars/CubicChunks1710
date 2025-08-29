28128bda5dec2b95e7f502ad0a9e44f000991481
rjnasers <rjnasers@iastate.edu>
2025-08-29 12:51:04 +0200

    Pushing for debugging

--- src/main/java/com/cardinalstar/cubicchunks/mixin/Mixins.java
+++ src/main/java/com/cardinalstar/cubicchunks/mixin/Mixins.java
@@ -1,5 +1,6 @@
 package com.cardinalstar.cubicchunks.mixin;
 
+import com.cardinalstar.cubicchunks.mixin.early.common.MixinEntityTracker;
 import com.gtnewhorizon.gtnhmixins.builders.IMixins;
 import com.gtnewhorizon.gtnhmixins.builders.MixinBuilder;
 import scala.tools.nsc.transform.Mixin;
@@ -8,8 +9,40 @@ import javax.annotation.Nonnull;
 
 public enum Mixins implements IMixins {
 
+    //  =============================================================
+    //                        COMMON Mixins
+    //  =============================================================
+    // MISC
     MIXIN_ANVIL_SAVE_HANDLER(new MixinBuilder("Changing the save handler to return a Cubic anvil chunk loader.")
         .addCommonMixins("common.MixinAnvilSaveHandler")
+        .setPhase(Phase.EARLY)
+        .setApplyIf(() -> true)),
+    MIXIN_GAME_REGISTRY_ACCESSOR(new MixinBuilder("Allows access to the generators in GameRegistry")
+        .addCommonMixins("common.IGameRegistry")
+        .setPhase(Phase.EARLY)
+        .setApplyIf(() -> true)),
+
+    // CHUNK
+    MIXIN_CHUNK_COLUMN(new MixinBuilder("Mixin for making chunks into columns.")
+        .addCommonMixins("common.MixinChunk_Column")
+        .setPhase(Phase.EARLY)
+        .setApplyIf(() -> true)),
+    MIXIN_CHUNK_CUBES(new MixinBuilder("Mixin to make it so that chunk methods are redirected to cubes.")
+        .addCommonMixins("common.MixinChunk_Cubes")
+        .setPhase(Phase.EARLY)
+        .setApplyIf(() -> true)),
+    MIXIN_CHUNK_CACHE_HEIGHT_LIMITS(new MixinBuilder("Mixin to fix height limits in ChunkCache")
+        .addCommonMixins("common.MixinChunkCache_HeightLimits")
+        .setPhase(Phase.EARLY)
+        .setApplyIf(() -> true)),
+
+    // WORLD
+    MIXIN_WORLD_SERVER(new MixinBuilder("Mixin for making world server into a ICubicWorldInternal.Server")
+        .addCommonMixins("common.MixinWorldServer")
+        .setPhase(Phase.EARLY)
+        .setApplyIf(() -> true)),
+    MIXIN_WORLD_SETTINGS(new MixinBuilder("Mixin for world settings allowing cubes.")
+        .addCommonMixins("common.MixinWorldSettings")
         .setPhase(Phase.EARLY)
         .setApplyIf(() -> true)),
     MIXIN_WORLD_PROVIDER(new MixinBuilder("Implementing ICubicWorldProvider.")
@@ -20,14 +53,63 @@ public enum Mixins implements IMixins {
         .addCommonMixins("common.MixinWorld")
         .setPhase(Phase.EARLY)
         .setApplyIf(() -> true)),
-
+    MIXIN_WORLD_INFO(new MixinBuilder("Implementing ICubicWorldInfo")
+        .addCommonMixins("common.MixinWorldInfo")
+        .setPhase(Phase.EARLY)
+        .setApplyIf(() -> true)),
+    MIXIN_DERIVED_WORLD_INFO(new MixinBuilder("Giving the isCubic method to DerivedWorldInfo")
+        .addCommonMixins("common.MixinDerivedWorldInfo")
+        .setPhase(Phase.EARLY)
+        .setApplyIf(() -> true)),
+    MIXIN_BLOCK_ACCESS_MIN_MAX_FIX(new MixinBuilder("Have IBlockAccess implement IMinMaxHeight")
+        .addCommonMixins("common.MixinIBlockAccess_MinMaxHeight")
+        .setPhase(Phase.EARLY)
+        .setApplyIf(() -> true)),
+    MIXIN_WORLD_HEIGHT_LIMIT(new MixinBuilder("Fix a ton of height limit issues in World.")
+        .addCommonMixins("common.MixinWorld_HeightLimit")
+        .setPhase(Phase.EARLY)
+        .setApplyIf(() -> true)),
+    MIXIN_WORLD_TICK(new MixinBuilder("Redirecting some things to use Y values.")
+        .addCommonMixins("common.MixinWorld_Tick")
+        .setPhase(Phase.EARLY)
+        .setApplyIf(() -> true)),
+    // ENTITY
+    MIXIN_ENTITY_DEATH_FIX(new MixinBuilder("Replace -64 constant, to avoid killing entities below y=-64")
+        .addCommonMixins("common.MixinEntity_DeathFix")
+        .setPhase(Phase.EARLY)
+        .setApplyIf(() -> true)),
+    MIXIN_ENTITY_TRACKER(new MixinBuilder("Changing the EntityTracker to work with cubes.")
+        .addCommonMixins("common.MixinEntityTracker")
+        .setPhase(Phase.EARLY)
+        .setApplyIf(() -> true)),
+    MIXIN_ENTITY_TRACKER_ENTRY(new MixinBuilder("Changing entityTrackerEntry to use cubes.")
+        .addCommonMixins("common.MixinEntityTrackerEntry")
+        .setPhase(Phase.EARLY)
+        .setApplyIf(() -> true)),
+    MIXIN_SPAWNER_ANIMALS(new MixinBuilder("Fixing spawner animals to work with cubes.")
+        .addCommonMixins("common.MixinSpawnerAnimals")
+        .setPhase(Phase.EARLY)
+        .setApplyIf(() -> true)),
+    MIXIN_STRUCTURE_START(new MixinBuilder("Giving Y position to structures.")
+        .addCommonMixins("common.MixinStructureStart")
+        .setPhase(Phase.EARLY)
+        .setApplyIf(() -> true)),
+    // SERVER
     MIXIN_INTEGRATED_SERVER_ACCESSOR(new MixinBuilder("Allows access to the worldsettings field.")
         .addCommonMixins("common.IIntegratedServer")
         .setPhase(Phase.EARLY)
         .setApplyIf(() -> true)),
-
-    MIXIN_WORLD_SETTINGS(new MixinBuilder("Mixin for world settings allowing cubes.")
-        .addCommonMixins("common.MixinWorldSettings")
+    MIXIN_MINECRAFT_SERVER(new MixinBuilder("Initializes a cubic world instead of a normal world.")
+        .addCommonMixins("common.MixinMinecraftServer")
+        .setPhase(Phase.EARLY)
+        .setApplyIf(() -> true)),
+    MIXIN_SERVER_CONFIGURATION_MANAGER(new MixinBuilder("Implements ICubicPlayerList in ServerConfigurationManager")
+        .addCommonMixins("common.MixinServerConfigurationManager")
+        .setPhase(Phase.EARLY)
+        .setApplyIf(() -> true)),
+    MIXIN_REGION_FILE_CACHE(new MixinBuilder("I believe this is for compat but IDK")
+        .addCommonMixins("common.MixinRegionFileCache")
+        .setPhase(Phase.EARLY)
         .setApplyIf(() -> true)),
 
     //  =============================================================
@@ -45,10 +127,20 @@ public enum Mixins implements IMixins {
     MIXIN_IGUI_SCREEN(new MixinBuilder("Allows access to the buttonList field.")
         .addClientMixins("client.IGuiScreen")
         .setPhase(Phase.EARLY)
+        .setApplyIf(() -> true)),
+
+    //  =============================================================
+    //                        Server Mixins
+    //  =============================================================
+
+    MIXIN_DEDICATED_PLAYER_LIST(new MixinBuilder("Allow to set vertical view distance.")
+        .addServerMixins("server.MixinDedicatedPlayerList")
+        .setPhase(Phase.EARLY)
+        .setApplyIf(() -> true)),
+    MIXIN_DEDICATED_SERVER_HEIGHT_LIMITS(new MixinBuilder("Fixing height limit issues in dedicated server.")
+        .addServerMixins("server.MixinDedicatedServer_HeightLimits")
+        .setPhase(Phase.EARLY)
         .setApplyIf(() -> true));
-
-
-
 
     private final MixinBuilder builder;
 
