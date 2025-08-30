f5b7fc3c74e18089137a3633757befbe861c8ac6
Fabian Maurer <dark.shadow4@web.de>
2025-08-31 01:28:57 +0200

    mark world type as cubic

--- src/main/java/com/cardinalstar/cubicchunks/world/type/VanillaCubicWorldType.java
+++ src/main/java/com/cardinalstar/cubicchunks/world/type/VanillaCubicWorldType.java
@@ -61,6 +61,6 @@ public class VanillaCubicWorldType extends WorldType implements ICubicWorldType 
 
     @Override
     public boolean hasCubicGeneratorForWorld(World object) {
-        return false;
+        return true;
     }
 }
