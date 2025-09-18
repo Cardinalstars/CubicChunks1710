160bd7423cdced4be5d5f85553fd5e8f3aa6a487
rjnasers <rjnasers@iastate.edu>
2025-09-18 23:07:06 +0200

    Fixing server issues

--- src/main/java/com/cardinalstar/cubicchunks/network/AbstractClientMessageHandler.java
+++ src/main/java/com/cardinalstar/cubicchunks/network/AbstractClientMessageHandler.java
@@ -26,8 +26,12 @@ import net.minecraft.entity.player.EntityPlayer;
 
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 @ParametersAreNonnullByDefault
+
+@SideOnly(Side.CLIENT)
 public abstract class AbstractClientMessageHandler<T extends IMessage> extends AbstractMessageHandler<T> {
 
     // implementing a final version of the server message handler both prevents it from
--- src/main/java/com/cardinalstar/cubicchunks/network/AbstractMessageHandler.java
+++ src/main/java/com/cardinalstar/cubicchunks/network/AbstractMessageHandler.java
@@ -35,7 +35,6 @@ import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 
-// TODO Maybe thread this?
 @ParametersAreNonnullByDefault
 public abstract class AbstractMessageHandler<T extends IMessage> implements IMessageHandler<T, IMessage> {
 
