2a3f7bbe6431bb473fd809c8bb6287288b8b87ca
Jakob Erber <90445342+Jakfut@users.noreply.github.com>
2026-09-05 19:26:28 +0200

    Fix block interaction outside vanilla height limits (#118)
    * Fix block interaction outside vanilla height limits
    
    * Fix C08 mixin registration after merge

--- src/main/java/com/cardinalstar/cubicchunks/mixin/Mixins.java
+++ src/main/java/com/cardinalstar/cubicchunks/mixin/Mixins.java
@@ -72,6 +72,14 @@ public enum Mixins implements IMixins {
         .addCommonMixins("common.vanillaclient.MixinS01PacketJoinGame")
         .setPhase(Phase.EARLY)
         .setApplyIf(() -> true)),
+    MIXIN_C07_HEIGHT_LIMITS(new MixinBuilder("Changing packet C07 to read and write full integer Y values.")
+        .addCommonMixins("common.MixinC07PacketPlayerDigging")
+        .setPhase(Phase.EARLY)
+        .setApplyIf(() -> true)),
+    MIXIN_C08_HEIGHT_LIMITS(new MixinBuilder("Changing packet C08 to read and write full integer Y values.")
+        .addCommonMixins("common.MixinC08PacketPlayerBlockPlacement")
+        .setPhase(Phase.EARLY)
+        .setApplyIf(() -> true)),
     MIXIN_S28_HEIGHT_LIMITS(new MixinBuilder("Changing packet S28 to read and write full integer Y values.")
         .addCommonMixins("common.MixinS28PacketEffect")
         .setPhase(Phase.EARLY)
